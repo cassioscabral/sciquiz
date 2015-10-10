f9e2547c97331daba7e35a9136737ebd8a537903
Dominike <ediko.dominike@gmail.com>
2015-10-10 19:12:28 -0300

    add user validation to quiz page

--- both/components/quiz_page.jsx
+++ both/components/quiz_page.jsx
@@ -6,14 +6,14 @@ QuizPage = React.createClass({
         return data;
     },
     getContent() {
-        return <form ref="form">
+        return <form>
             <a href="/">Back</a>
             <h3>{this.data.quiz.title}</h3>
             {this.data.quiz.questions.map(function(question) {
-                return <div>
+                return <div ref={question._id}>
                     <p>{question.title}</p>
                     {question.answers.map(function(answer) {
-                        return <input type="radio" name={question.title} value={answer.value}>{answer.title}<br/></input>
+                        return <input type="radio" ref={answer._id} name={question.title} value={answer.value}>{answer.title}<br/></input>
                     })}
                 </div>
             })}
@@ -22,11 +22,35 @@ QuizPage = React.createClass({
             </button>
         </form>;
     },
-    getQuestions(){
-
+    sumScore(){
+        sum = 0;
+        for (q in this.data.quiz.questions){
+            for (a in this.data.quiz.questions[q].answers){
+                answerID = this.data.quiz.questions[q].answers[a]._id;
+                answerValue = parseInt(React.findDOMNode(this.refs[answerID]).value);
+                answerChecked = React.findDOMNode(this.refs[answerID]).checked;
+                if (answerChecked){
+                    sum = sum + answerValue;
+                }
+            }
+        }
+        return sum;
     },
     save(event){
         event.preventDefault();
+        score = this.sumScore();
+        quizID = this.data.quiz._id;
+        userId = Meteor.userId();
+        previusValue = this.data.quiz.users[userId];
+        var setModifier = { $set: {} };
+        setModifier.$set['users.' + userId] = score;
+        Quizzes.update(quizID,setModifier);
+
+        if (previusValue){
+            score = score - previusValue;
+        }
+
+        Quizzes.update(quizID,{ $inc: { maxScore: score} });
     },
     render() {
         return (this.data.quiz)? this.getContent() : <div>loading...</div>;
