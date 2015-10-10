522ef7444b7e623b16cbb0ce3d82088e7a97ebd6
Dominike <ediko.dominike@gmail.com>
2015-10-10 09:49:44 -0300

    fix facebook connection

--- sciquiz.jsx
+++ sciquiz.jsx
@@ -13,16 +13,15 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-    ServiceConfiguration.configurations.upsert(
-        { service: "facebook" },
-        {
-            $set: {
-                clientId: "clientId",
-                loginStyle: "popup",
-                secret: "secretId"
-            }
-        }
-    );
+    ServiceConfiguration.configurations.remove({
+        service: 'facebook'
+    });
+ 
+    ServiceConfiguration.configurations.insert({
+        service: 'facebook',
+        appId: '1001129963234030',
+        secret: '81176a1122c93513c28585b244130126'
+    });
 }
 
 Meteor.methods({
