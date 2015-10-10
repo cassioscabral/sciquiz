d917bb83084040b814b8f85529e11cac6ba3e0c8
Dominike <ediko.dominike@gmail.com>
2015-10-10 16:24:46 -0300

    complete quiz collection

--- both/collections.jsx
+++ both/collections.jsx
@@ -9,23 +9,23 @@ if(Meteor.isServer) {
     Answers.remove({});
 
     answer1_1 = Answers.insert({
-        title: 'I have not stopped trying to get what I want',
+        title: "I have not stopped trying to get what I want",
         value: 0
     });
     answer2_1 = Answers.insert({
-        title: 'I have stopped trying to get what I want in some situations',
+        title: "I have stopped trying to get what I want in some situations",
         value: 1
     });
     answer3_1 = Answers.insert({
-        title: 'I have stopped trying to get what I want in most situations',
+        title: "I have stopped trying to get what I want in most situations",
         value: 2
     });
     answer4_1 = Answers.insert({
-        title: 'I have stopped trying to get what I want in all situations',
+        title: "I have stopped trying to get what I want in all situations",
         value: 3
     });
     question1_1 = Questions.insert({
-        title:'Motivational Deficit 1',
+        title:"Motivational Deficit 1",
         answers:[
             Answers.findOne(answer1_1),
             Answers.findOne(answer2_1),
@@ -34,39 +34,94 @@ if(Meteor.isServer) {
         ]
     });
 
+    answer1_2 = Answers.insert({
+        title: "I am not passive when it comes to getting what I want these days",
+        value: 0
+    });
+    answer2_2 = Answers.insert({
+        title: "In some situations I'm passive when it comes to getting what I want",
+        value: 1
+    });
+    answer3_2 = Answers.insert({
+        title: "In most situations I'm passive when it comes to getting what I want",
+        value: 2
+    });
+    answer4_2 = Answers.insert({
+        title: "In all situations I'm passive when it comes to getting what I want these days",
+        value: 3
+    });
+    question2_1 = Questions.insert({
+        title:"Motivational Deficit 2",
+        answers:[
+            Answers.findOne(answer1_2),
+            Answers.findOne(answer2_2),
+            Answers.findOne(answer3_2),
+            Answers.findOne(answer4_2)
+        ]
+    });
+
+
+    answer1_3 = Answers.insert({
+        title: "I have not given up trying to accomplish what's important to me",
+        value: 0
+    });
+    answer2_3 = Answers.insert({
+        title: "I have given up trying to accomplish some things that are important to me",
+        value: 1
+    });
+    answer3_3 = Answers.insert({
+        title: "I have given up trying to accomplish most things that are important to me",
+        value: 2
+    });
+    answer4_3 = Answers.insert({
+        title: "I have given up trying to accomplish all things that are important to me",
+        value: 3
+    });
+    question3_1 = Questions.insert({
+        title:'Motivational Deficit 3',
+        answers:[
+            Answers.findOne(answer1_3),
+            Answers.findOne(answer2_3),
+            Answers.findOne(answer3_3),
+            Answers.findOne(answer4_3)
+        ]
+    });
+
+    answer1_4 = Answers.insert({
+        title: "My motivation to get things done is as good as usual",
+        value: 0
+    });
+    answer2_4 = Answers.insert({
+        title: "In some situations, my motivation to get things done is lower than usual",
+        value: 1
+    });
+    answer3_4 = Answers.insert({
+        title: "In most situations my motivation to get things done is lower than usual",
+        value: 2
+    });
+    answer4_4 = Answers.insert({
+        title: "In all situations my motivation to get things done is lower than usual",
+        value: 3
+    });
+    question4_1 = Questions.insert({
+        title:'Motivational Deficit 4',
+        answers:[
+            Answers.findOne(answer1_4),
+            Answers.findOne(answer2_4),
+            Answers.findOne(answer3_4),
+            Answers.findOne(answer4_4)
+        ]
+    });
+
     Quizzes.insert({
         title: 'Motivational Deficit',
         name: 'MotivationalDeficit',
         type: 'scoredQuiz',
         questions:[
-            Questions.findOne(question1_1)
+            Questions.findOne(question1_1),
+            Questions.findOne(question2_1),
+            Questions.findOne(question3_1),
+            Questions.findOne(question4_1),
         ]
     });
 }
-
-
-// 1.
-// 0= I have not stopped trying to get what I want.
-// 1=I have stopped trying to get what I want in some situations.
-// 2= I have stopped trying to get what I want in most situations.
-// 3= I have stopped trying to get what I want in all situations.
-// 2.
-// 0= I am not passive when it comes to getting what I want these days.
-// 1= In some situations I'm passive when it comes to getting what I want
-// these days.
-// 2= In most situations I'm passive when it comes to getting what I want
-// these days.
-// 3= In all situations I'm passive when it comes to getting what I want these days.
-// 3.
-// 0= I have not given up trying to accomplish what's important to me.
-// 1= I have given up trying to accomplish some things that are important
-// to me.
-// 2= I have given up trying to accomplish most things that are important to
-// me.
-// 3= I have given up trying to accomplish all things that are important to
-// me.
-// 4.
-// 0= My motivation to get things done is as good as usual.
-// 1= In some situations, my motivation to get things done is lower than usual.
-// 2= In most situations my motivation to get things done is lower than usual.
-// 3= In all situations my motivation to get things done is lower than usual
