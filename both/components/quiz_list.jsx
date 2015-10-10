eb3f2738727e2dff43b4c8d3d6ce5fd971e21c20
Dominike <ediko.dominike@gmail.com>
2015-10-10 16:16:26 -0300

    change quiz pages

--- both/components/quiz_list.jsx
+++ both/components/quiz_list.jsx
@@ -9,13 +9,13 @@ QuizList = React.createClass({
         return <ul>
             {this.data.quizzes.map(function(quiz) {
                 var path = FlowRouter.path('quiz', {_id: quiz._id})
-                return <li key={quiz._id}><a href={path}>{quiz.title}</a></li>
+                return <li key={quiz._id}><a href={path}>{quiz.title+" - "+quiz.type}</a></li>
             })}
         </ul>;
     },
     render() {
         return <div>
-            This is the quiz list
+            <h1>Quizzes</h1>
             {(this.data.quizzes)? this.getList() : "loading..."}
         </div>;
     }
--- both/components/quiz_page.jsx
+++ both/components/quiz_page.jsx
@@ -5,10 +5,23 @@ QuizPage = React.createClass({
         data.quiz = Quizzes.findOne({_id: this.props._id});
         return data;
     },
+    getQuestions(){
+        return <div>
+            {this.data.quiz.questions.map(function(question) {
+                return <form>
+                    <p>{question.title}</p>
+                    {question.answers.map(function(answer) {
+                        return <input type="radio" name={question.title} value={answer.value}>{answer.title}<br/></input>
+                    })}
+                </form>
+            })}
+            </div>;
+    },
     getContent() {
         return <div>
             <a href="/">Back</a>
             <h3>{this.data.quiz.title}</h3>
+            {this.getQuestions()}
         </div>;
     },
     render() {
